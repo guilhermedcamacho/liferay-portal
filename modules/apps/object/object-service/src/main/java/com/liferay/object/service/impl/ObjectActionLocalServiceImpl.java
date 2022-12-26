bf9b82beb8a60d6f79f3f51b7a38d8f361a105e6
Guilherme Camacho <guilherme.camacho@liferay.com>
2022-12-25 21:23:20 -0300

    LPS-169369 Inline and Sort

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectActionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectActionLocalServiceImpl.java
@@ -102,8 +102,9 @@ public class ObjectActionLocalServiceImpl
 		User user = _userLocalService.getUser(userId);
 
 		_validateParametersUnicodeProperties(
-			user.getCompanyId(), conditionExpression, objectActionExecutorKey,
-			objectActionTriggerKey, parametersUnicodeProperties, userId);
+			user.getCompanyId(), userId, conditionExpression,
+			objectActionExecutorKey, objectActionTriggerKey,
+			parametersUnicodeProperties);
 
 		ObjectAction objectAction = objectActionPersistence.create(
 			counterLocalService.increment());
@@ -198,9 +199,9 @@ public class ObjectActionLocalServiceImpl
 			objectActionId);
 
 		_validateParametersUnicodeProperties(
-			objectAction.getCompanyId(), conditionExpression,
-			objectActionExecutorKey, objectActionTriggerKey,
-			parametersUnicodeProperties, objectAction.getUserId());
+			objectAction.getCompanyId(), objectAction.getUserId(),
+			conditionExpression, objectActionExecutorKey,
+			objectActionTriggerKey, parametersUnicodeProperties);
 
 		objectAction.setActive(active);
 		objectAction.setConditionExpression(conditionExpression);
@@ -359,9 +360,9 @@ public class ObjectActionLocalServiceImpl
 	}
 
 	private void _validateParametersUnicodeProperties(
-			long companyId, String conditionExpression,
+			long companyId, long userId, String conditionExpression,
 			String objectActionExecutorKey, String objectActionTriggerKey,
-			UnicodeProperties parametersUnicodeProperties, long userId)
+			UnicodeProperties parametersUnicodeProperties)
 		throws PortalException {
 
 		Map<String, Object> errorMessageKeys = new HashMap<>();
