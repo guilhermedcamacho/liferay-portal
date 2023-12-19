6190d9027dc0bdf062b2dc2a96689dd32f41ca13
Guilherme Camacho <guilherme.camacho@liferay.com>
2023-12-18 21:39:24 -0300

    Revert "LPS-198290 Remove programatically the object entries before anything else to avoid TestRules execute other deletions"
    This reverts commit 447c6cc556b8b6ecd1bbf5316a7b437ad5b22e88.

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/manager/v1_0/test/DefaultObjectEntryManagerImplTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/manager/v1_0/test/DefaultObjectEntryManagerImplTest.java
@@ -58,6 +58,7 @@ import com.liferay.object.rest.manager.v1_0.DefaultObjectEntryManager;
 import com.liferay.object.rest.manager.v1_0.ObjectEntryManager;
 import com.liferay.object.rest.test.util.BaseObjectEntryManagerImplTestCase;
 import com.liferay.object.rest.test.util.ObjectRelationshipTestUtil;
+import com.liferay.object.service.ObjectEntryLocalService;
 import com.liferay.object.service.ObjectFieldService;
 import com.liferay.object.service.ObjectFieldSettingLocalService;
 import com.liferay.object.service.ObjectFilterLocalService;
@@ -178,8 +179,6 @@ public class DefaultObjectEntryManagerImplTest
 
 	@BeforeClass
 	public static void setUpClass() throws Exception {
-		BaseObjectEntryManagerImplTestCase.setUpClass();
-
 		companyId = TestPropsValues.getCompanyId();
 		_defaultObjectEntryManager =
 			(DefaultObjectEntryManager)_objectEntryManager;
@@ -187,7 +186,6 @@ public class DefaultObjectEntryManagerImplTest
 		_originalName = PrincipalThreadLocal.getName();
 		_originalPermissionChecker =
 			PermissionThreadLocal.getPermissionChecker();
-
 		_simpleDateFormat = DateFormatFactoryUtil.getSimpleDateFormat(
 			"yyyy-MM-dd");
 
@@ -205,8 +203,6 @@ public class DefaultObjectEntryManagerImplTest
 
 	@AfterClass
 	public static void tearDownClass() throws Exception {
-		BaseObjectEntryManagerImplTestCase.tearDownClass();
-
 		GroupLocalServiceUtil.deleteGroup(_group);
 
 		PermissionThreadLocal.setPermissionChecker(_originalPermissionChecker);
@@ -541,15 +537,13 @@ public class DefaultObjectEntryManagerImplTest
 
 	@After
 	public void tearDown() throws Exception {
-		super.tearDown();
-
 		NestedFieldsContextThreadLocal.setNestedFieldsContext(
 			_originalNestedFieldsContext);
 
 		TreeTestUtil.deleteObjectDefinitionHierarchy(
 			objectDefinitionLocalService,
 			new String[] {"C_A", "C_AA", "C_AB", "C_AAA", "C_AAB"},
-			objectEntryLocalService);
+			_objectEntryLocalService);
 	}
 
 	@Test
@@ -1677,7 +1671,7 @@ public class DefaultObjectEntryManagerImplTest
 
 		TreeTestUtil.forEachNodeObjectEntry(
 			tree.iterator(TreeConstants.ITERATOR_TYPE_POST_ORDER),
-			objectEntryLocalService,
+			_objectEntryLocalService,
 			objectEntry -> {
 				ObjectDefinition objectDefinition =
 					objectDefinitionLocalService.fetchObjectDefinition(
@@ -1699,7 +1693,7 @@ public class DefaultObjectEntryManagerImplTest
 
 		TreeTestUtil.forEachNodeObjectEntry(
 			tree.iterator(TreeConstants.ITERATOR_TYPE_POST_ORDER),
-			objectEntryLocalService,
+			_objectEntryLocalService,
 			objectEntry -> {
 				ObjectDefinition objectDefinition =
 					objectDefinitionLocalService.fetchObjectDefinition(
@@ -2657,7 +2651,7 @@ public class DefaultObjectEntryManagerImplTest
 
 		AccountEntry accountEntry1 = _addAccountEntry();
 
-		objectEntryLocalService.addObjectEntry(
+		_objectEntryLocalService.addObjectEntry(
 			adminUser.getUserId(), 0,
 			_objectDefinition3.getObjectDefinitionId(),
 			HashMapBuilder.<String, Serializable>put(
@@ -2672,7 +2666,7 @@ public class DefaultObjectEntryManagerImplTest
 
 		AccountEntry accountEntry2 = _addAccountEntry();
 
-		objectEntryLocalService.addObjectEntry(
+		_objectEntryLocalService.addObjectEntry(
 			adminUser.getUserId(), 0,
 			_objectDefinition3.getObjectDefinitionId(),
 			HashMapBuilder.<String, Serializable>put(
@@ -3104,7 +3098,7 @@ public class DefaultObjectEntryManagerImplTest
 		Node rootNode = tree.getRootNode();
 
 		TreeTestUtil.forEachNodeObjectEntry(
-			tree.iterator(), objectEntryLocalService,
+			tree.iterator(), _objectEntryLocalService,
 			objectEntry -> {
 				ObjectDefinition objectDefinition =
 					objectDefinitionLocalService.fetchObjectDefinition(
@@ -3129,7 +3123,7 @@ public class DefaultObjectEntryManagerImplTest
 			ActionKeys.UPDATE, _rootObjectDefinition, _buyerRole);
 
 		TreeTestUtil.forEachNodeObjectEntry(
-			tree.iterator(), objectEntryLocalService,
+			tree.iterator(), _objectEntryLocalService,
 			objectEntry -> {
 				ObjectDefinition objectDefinition =
 					objectDefinitionLocalService.fetchObjectDefinition(
@@ -3645,14 +3639,14 @@ public class DefaultObjectEntryManagerImplTest
 		throws Exception {
 
 		Tree tree = TreeTestUtil.createObjectEntryTree(
-			externalReferenceCodeSuffix, objectEntryLocalService,
+			externalReferenceCodeSuffix, _objectEntryLocalService,
 			objectFieldLocalService,
 			_rootObjectDefinition.getObjectDefinitionId(),
 			_objectRelationshipLocalService, _treeFactory);
 
 		Node node = tree.getRootNode();
 
-		objectEntryLocalService.updateObjectEntry(
+		_objectEntryLocalService.updateObjectEntry(
 			adminUser.getUserId(), node.getPrimaryKey(),
 			HashMapBuilder.<String, Serializable>put(
 				() -> {
@@ -3833,6 +3827,9 @@ public class DefaultObjectEntryManagerImplTest
 
 	@DeleteAfterTestRun
 	private ObjectDefinition _objectDefinition3;
+
+	@Inject
+	private ObjectEntryLocalService _objectEntryLocalService;
 
 	@Inject
 	private ObjectFieldService _objectFieldService;
