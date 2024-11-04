3e7901f84245b1cfe9873a0857558afef9fa6f05
Guilherme Camacho <guilherme.camacho@liferay.com>
2024-11-04 15:32:42 -0300

    LPD-39807 SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/exception/DuplicateDDMStructureExternalReferenceCodeException.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/exception/DuplicateDDMStructureExternalReferenceCodeException.java
@@ -2,6 +2,7 @@
  * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+
 package com.liferay.dynamic.data.mapping.exception;
 
 import com.liferay.portal.kernel.exception.DuplicateExternalReferenceCodeException;
@@ -9,7 +10,8 @@ import com.liferay.portal.kernel.exception.DuplicateExternalReferenceCodeExcepti
 /**
  * @author Brian Wing Shun Chan
  */
-public class DuplicateDDMStructureExternalReferenceCodeException extends DuplicateExternalReferenceCodeException {
+public class DuplicateDDMStructureExternalReferenceCodeException
+	extends DuplicateExternalReferenceCodeException {
 
 	public DuplicateDDMStructureExternalReferenceCodeException() {
 	}
@@ -18,11 +20,15 @@ public class DuplicateDDMStructureExternalReferenceCodeException extends Duplica
 		super(msg);
 	}
 
-	public DuplicateDDMStructureExternalReferenceCodeException(String msg, Throwable throwable) {
+	public DuplicateDDMStructureExternalReferenceCodeException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 
-	public DuplicateDDMStructureExternalReferenceCodeException(Throwable throwable) {
+	public DuplicateDDMStructureExternalReferenceCodeException(
+		Throwable throwable) {
+
 		super(throwable);
 	}
 
