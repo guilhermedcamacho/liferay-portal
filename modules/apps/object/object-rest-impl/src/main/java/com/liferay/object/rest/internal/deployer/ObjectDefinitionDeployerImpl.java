08e8123b9e454cc03c982411adcddc4f6c9bca80
Guilherme Camacho <guilherme.camacho@liferay.com>
2025-02-21 01:52:10 -0300

    LPD-45146 Simplify and reuse ConfigurationUtil.getExcludedOperationIds method

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/deployer/ObjectDefinitionDeployerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/deployer/ObjectDefinitionDeployerImpl.java
@@ -67,7 +67,6 @@ import com.liferay.portal.kernel.service.ResourceActionLocalService;
 import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
 import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
-import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.MapUtil;
@@ -76,6 +75,7 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.odata.filter.ExpressionConvert;
 import com.liferay.portal.odata.filter.FilterParserProvider;
 import com.liferay.portal.odata.sort.SortParserProvider;
+import com.liferay.portal.vulcan.configuration.util.ConfigurationUtil;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
 import com.liferay.portal.vulcan.extension.ExtensionProviderRegistry;
@@ -89,9 +89,11 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Dictionary;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Supplier;
 
@@ -271,17 +273,9 @@ public class ObjectDefinitionDeployerImpl implements ObjectDefinitionDeployer {
 		ObjectScopeProvider objectScopeProvider) {
 
 		try {
-			String factoryPid =
-				"com.liferay.portal.vulcan.configuration." +
-					"VulcanCompanyConfiguration";
-
-			Configuration configuration = _getConfiguration(
-				objectDefinition.getCompanyId(), factoryPid,
-				objectDefinition.getRESTContextPath());
-
 			Method[] methods = BaseObjectEntryResourceImpl.class.getMethods();
 
-			List<String> excludedOperationIds = new ArrayList<>();
+			Set<String> excludedOperationIds = new HashSet<>();
 
 			for (Method method : methods) {
 				Path path = method.getAnnotation(Path.class);
@@ -305,28 +299,22 @@ public class ObjectDefinitionDeployerImpl implements ObjectDefinitionDeployer {
 				}
 			}
 
-			if (configuration != null) {
-				Dictionary<String, Object> properties =
-					configuration.getProperties();
+			Set<String> currentExcludedOperationIds =
+				ConfigurationUtil.getExcludedOperationIds(
+					objectDefinition.getCompanyId(), _configurationAdmin,
+					objectDefinition.getRESTContextPath());
 
-				List<String> currentExcludedOperationIds = ListUtil.fromString(
-					GetterUtil.getString(
-						properties.get("excludedOperationIds")),
-					StringPool.COMMA);
+			if (Objects.equals(
+					currentExcludedOperationIds, excludedOperationIds)) {
 
-				Collections.sort(currentExcludedOperationIds);
-
-				Collections.sort(excludedOperationIds);
-
-				if (Objects.equals(
-						currentExcludedOperationIds, excludedOperationIds)) {
-
-					return;
-				}
+				return;
 			}
 
-			configuration = _configurationAdmin.createFactoryConfiguration(
-				factoryPid, StringPool.QUESTION);
+			Configuration configuration =
+				_configurationAdmin.createFactoryConfiguration(
+					"com.liferay.portal.vulcan.configuration." +
+						"VulcanCompanyConfiguration",
+					StringPool.QUESTION);
 
 			configuration.update(
 				HashMapDictionaryBuilder.put(
@@ -345,36 +333,6 @@ public class ObjectDefinitionDeployerImpl implements ObjectDefinitionDeployer {
 				_log.debug(exception);
 			}
 		}
-	}
-
-	private Configuration _getConfiguration(
-		long companyId, String factoryPid, String path) {
-
-		try {
-			String filterString = String.format(
-				"(&(path=%s)(|(service.factoryPid=%s)" +
-					"(&(service.factoryPid=%s)(%s=%d))))",
-				path,
-				"com.liferay.portal.vulcan.internal.configuration." +
-					"VulcanConfiguration",
-				factoryPid,
-				ExtendedObjectClassDefinition.Scope.COMPANY.getPropertyKey(),
-				companyId);
-
-			Configuration[] configurations =
-				_configurationAdmin.listConfigurations(filterString);
-
-			if (configurations != null) {
-				return configurations[0];
-			}
-		}
-		catch (Exception exception) {
-			if (_log.isDebugEnabled()) {
-				_log.debug(exception);
-			}
-		}
-
-		return null;
 	}
 
 	private String _getEntityClassName(ObjectDefinition objectDefinition) {
