6242a1462cc2c6941a417f16eeb0a4ef06c3c2bb
Jaime Leon <jaime.leon@liferay.com>
2025-04-01 12:58:51 +0200

    LPD-51573 Add web content to  8080 page

--- modules/test/playwright/tests/export-import-service/remoteStaging.spec.ts
+++ modules/test/playwright/tests/export-import-service/remoteStaging.spec.ts
@@ -3,15 +3,22 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {mergeTests} from '@playwright/test';
+import {expect, mergeTests} from '@playwright/test';
 
 import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
 import {dataApiHelpersTest} from '../../fixtures/dataApiHelpersTest';
 import {featureFlagsTest} from '../../fixtures/featureFlagsTest';
 import {loginTest} from '../../fixtures/loginTest';
+import {pageEditorPagesTest} from '../../fixtures/pageEditorPagesTest';
+import {pagesAdminPagesTest} from '../../fixtures/pagesAdminPagesTest';
+import {productMenuPageTest} from '../../fixtures/productMenuPageTest';
 import {remoteDataApiHelpersTest} from '../../fixtures/remoteDataApiHelpersTest';
 import {remoteStagingApiHelperTest} from '../../fixtures/remoteStagingApiHelpersTest';
+import {uiElementsPageTest} from '../../fixtures/uiElementsTest';
+import {webContentDisplayPageTest} from '../../fixtures/webContentDisplayPageTest';
 import {performLoginViaApi} from '../../utils/performLogin';
+import getBasicWebContentStructureId from '../../utils/structured-content/getBasicWebContentStructureId';
+import {pagesPagesTest} from '../layout-admin-web/fixtures/pagesPagesTest';
 import {remoteStagingPagesTest} from './fixtures/remoteStagingPagesTest';
 
 export const test = mergeTests(
@@ -21,9 +28,15 @@ export const test = mergeTests(
 	featureFlagsTest({
 		'LPD-39304': {enabled: true},
 	}),
+	pageEditorPagesTest,
+	pagesAdminPagesTest,
+	pagesPagesTest,
+	productMenuPageTest,
 	remoteDataApiHelpersTest,
 	remoteStagingApiHelperTest,
 	remoteStagingPagesTest,
+	uiElementsPageTest,
+	webContentDisplayPageTest
 );
 
 test(
@@ -32,8 +45,11 @@ test(
 	async ({
 		apiHelpers,
 		page,
+		pageEditorPage,
 		remoteStagingApiHelper,
 		remoteStagingPage,
+		uiElementsPage,
+		webContentDisplayPage,
 	}) => {
 		const site = await apiHelpers.headlessSite.createSite({
 			name: 'Site Name',
@@ -76,6 +92,32 @@ test(
 			groupId: site.id,
 			remoteGroupId: remoteSite.id,
 			remotePort: 9080,
+		});
+
+		await remoteStagingPage.publishToLive({
+			layoutFriendlyURL: layout.friendlyURL,
+			siteFriendlyUrl: site.friendlyUrlPath,
+		});
+
+		const basicWebContentStructureId =
+			await getBasicWebContentStructureId(apiHelpers);
+
+		await apiHelpers.jsonWebServicesJournal.addWebContent({
+			content: 'WC WebContent Content',
+			ddmStructureId: basicWebContentStructureId,
+			groupId: site.id,
+			titleMap: {en_US: 'WC WebContent Title'},
+		});
+
+		await pageEditorPage.goto(layout, site.friendlyUrlPath);
+		await uiElementsPage.addButton.click();
+		await pageEditorPage.addWidgetToWidgetPageTemplate(
+			'Content Management',
+			'Web Content Display'
+		);
+		await webContentDisplayPage.addWebContentWithDisplay({
+			pageType: 'widget',
+			webContentName: 'WC WebContent Title',
 		});
 
 		await remoteStagingPage.publishToLive({
