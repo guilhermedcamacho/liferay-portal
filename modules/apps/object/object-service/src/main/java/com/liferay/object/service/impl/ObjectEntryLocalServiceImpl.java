677cf37c8496a3e6cd49abb7c76a3ce57f29c8e5
Carlos Correa <carlos.correa@liferay.com>
2025-05-09 12:31:50 +0200

    LPD-53245 We only need to take the original value for partialUpdated if the whole map of localized values is missing, otherwise the missing value must be unset as before

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -3335,9 +3335,10 @@ public class ObjectEntryLocalServiceImpl
 	private Object _getLocalizedValue(
 		String languageId, Map<String, Object> localizedValues) {
 
-		if ((localizedValues == null) ||
-			!localizedValues.containsKey(languageId)) {
-
+		if (localizedValues == null) {
+			return null;
+		}
+		else if (!localizedValues.containsKey(languageId)) {
 			return StringPool.BLANK;
 		}
 
@@ -4292,14 +4293,16 @@ public class ObjectEntryLocalServiceImpl
 						(Map<String, Object>)values.get(
 							objectField.getI18nObjectFieldName()));
 
-					if (Validator.isBlank(
-							GetterUtil.getString(localizedValue)) &&
-						partialUpdate) {
+					if (localizedValue == null) {
+						if (partialUpdate) {
+							localizedValue = _getLocalizedValue(
+								languageId,
+								(Map<String, Object>)originalValues.get(
+									objectField.getI18nObjectFieldName()));
+						}
 
-						localizedValue = _getLocalizedValue(
-							languageId,
-							(Map<String, Object>)originalValues.get(
-								objectField.getI18nObjectFieldName()));
+						localizedValue = GetterUtil.getObject(
+							localizedValue, StringPool.BLANK);
 					}
 
 					_setColumn(
