a5f214af629f0fc186150e1ab028f1c9f6a94fb9
Brian Chan <brian.chan@liferay.com>
2025-05-20 05:41:35 -0300

    LPD-53245 Sort

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
@@ -824,8 +824,8 @@ public class DefaultObjectEntryManagerImpl
 				objectDefinition.getObjectDefinitionId(), objectEntry);
 
 		return _updateObjectEntry(
-			dtoConverterContext, objectDefinition, objectEntryId,
-			existingObjectEntry, true);
+			dtoConverterContext, objectDefinition, existingObjectEntry,
+			objectEntryId, true);
 	}
 
 	@Override
@@ -863,7 +863,7 @@ public class DefaultObjectEntryManagerImpl
 		throws Exception {
 
 		return _updateObjectEntry(
-			dtoConverterContext, objectDefinition, objectEntryId, objectEntry,
+			dtoConverterContext, objectDefinition, objectEntry, objectEntryId,
 			false);
 	}
 
@@ -2033,8 +2033,8 @@ public class DefaultObjectEntryManagerImpl
 
 	private ObjectEntry _updateObjectEntry(
 			DTOConverterContext dtoConverterContext,
-			ObjectDefinition objectDefinition, long objectEntryId,
-			ObjectEntry objectEntry, boolean partialUpdate)
+			ObjectDefinition objectDefinition, ObjectEntry objectEntry,
+			long objectEntryId, boolean partialUpdate)
 		throws Exception {
 
 		com.liferay.object.model.ObjectEntry serviceBuilderObjectEntry =
