5f941c0db93894b63de5cbcb003f110fb116508e
Crescenzo Rega <crescenzo.rega@liferay.com>
2025-05-28 15:18:45 +0200

    LPD-56135 Fixes issue with displaying prices for draft products

--- modules/apps/commerce/commerce-pricing-web/src/main/java/com/liferay/commerce/pricing/web/internal/display/context/CommercePriceEntryDisplayContext.java
+++ modules/apps/commerce/commerce-pricing-web/src/main/java/com/liferay/commerce/pricing/web/internal/display/context/CommercePriceEntryDisplayContext.java
@@ -33,6 +33,7 @@ import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -145,11 +146,9 @@ public class CommercePriceEntryDisplayContext
 
 		CommercePriceEntry commercePriceEntry = getCommercePriceEntry();
 
-		_cpInstance = _cpInstanceLocalService.getCProductInstance(
+		return _cpInstanceLocalService.fetchCPInstance(
 			commercePriceEntry.getCProductId(),
 			commercePriceEntry.getCPInstanceUuid());
-
-		return _cpInstance;
 	}
 
 	public List<CPInstanceUnitOfMeasure> getCPInstanceUnitOfMeasures()
@@ -157,9 +156,13 @@ public class CommercePriceEntryDisplayContext
 
 		CommercePriceEntry commercePriceEntry = getCommercePriceEntry();
 
-		CPInstance cpInstance = _cpInstanceLocalService.getCProductInstance(
+		CPInstance cpInstance = _cpInstanceLocalService.fetchCPInstance(
 			commercePriceEntry.getCProductId(),
 			commercePriceEntry.getCPInstanceUuid());
+
+		if (cpInstance == null) {
+			return Collections.emptyList();
+		}
 
 		return ListUtil.sort(
 			_cpInstanceUnitOfMeasureLocalService.
