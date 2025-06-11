5084a5349b793271a084300ccfbec8c29f2c424b
Jürgen Kappler <jurgen.kappler@liferay.com>
2025-06-11 18:22:23 +0200

    LPD-57224 Revert "LPD-56207 Sort"
    This reverts commit 230d3980abf62bd1faa293f98fc00c3040ef93b1.

--- modules/apps/document-library/document-library-test/src/testIntegration/java/com/liferay/document/library/internal/upgrade/v3_2_11/test/DLFileEntryTypesDDMStructureUpgradeProcessTest.java
+++ modules/apps/document-library/document-library-test/src/testIntegration/java/com/liferay/document/library/internal/upgrade/v3_2_11/test/DLFileEntryTypesDDMStructureUpgradeProcessTest.java
@@ -61,7 +61,7 @@ public class DLFileEntryTypesDDMStructureUpgradeProcessTest {
 		Company company = _companyLocalService.getCompany(
 			TestPropsValues.getCompanyId());
 
-		_companyGroup = company.getGroup();
+		_globalGroup = company.getGroup();
 
 		_group = GroupTestUtil.addGroup();
 	}
@@ -81,14 +81,14 @@ public class DLFileEntryTypesDDMStructureUpgradeProcessTest {
 		_runUpgrade();
 
 		_assertDDMStructure(
-			DDMStructureConstants.TYPE_AUTO, _companyGroup.getGroupId(),
-			"DL_VIDEO_EXTERNAL_SHORTCUT");
-		_assertDDMStructure(
-			DDMStructureConstants.TYPE_AUTO, _companyGroup.getGroupId(),
-			"GOOGLE_DOCS");
-		_assertDDMStructure(
 			DDMStructureConstants.TYPE_DEFAULT, _group.getGroupId(),
 			ddmStructure.getStructureKey());
+		_assertDDMStructure(
+			DDMStructureConstants.TYPE_AUTO, _globalGroup.getGroupId(),
+			"DL_VIDEO_EXTERNAL_SHORTCUT");
+		_assertDDMStructure(
+			DDMStructureConstants.TYPE_AUTO, _globalGroup.getGroupId(),
+			"GOOGLE_DOCS");
 	}
 
 	private DLFileEntryType _addFileEntryType() throws Exception {
@@ -150,7 +150,7 @@ public class DLFileEntryTypesDDMStructureUpgradeProcessTest {
 	@Inject
 	private EntityCache _entityCache;
 
-	private Group _companyGroup;
+	private Group _globalGroup;
 
 	@DeleteAfterTestRun
 	private Group _group;
