741a0fc15e283f1745686fe3f3ad0461ed0d948a
Guilherme Camacho <guilherme.camacho@liferay.com>
2025-06-17 00:04:49 -0300

    LPD-57356 Delete the validation

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceTest.java
@@ -2575,9 +2575,7 @@ public class ObjectEntryLocalServiceTest {
 					objectValidationRuleResults.size() - 1));
 		}
 
-		objectValidationRule5.setActive(false);
-
-		_objectValidationRuleLocalService.updateObjectValidationRule(
+		_objectValidationRuleLocalService.deleteObjectValidationRule(
 			objectValidationRule5);
 
 		// Must be over 18 years old
