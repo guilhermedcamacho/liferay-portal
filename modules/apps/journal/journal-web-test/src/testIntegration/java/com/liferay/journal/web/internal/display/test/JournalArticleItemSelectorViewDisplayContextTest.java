44a4e8719c2aa37ce97f0d85bf2c8864f5af1852
Jürgen Kappler <jurgen.kappler@liferay.com>
2025-06-19 15:04:36 +0200

    LPD-58219 Fix typo

--- modules/apps/journal/journal-web-test/src/testIntegration/java/com/liferay/journal/web/internal/display/test/JournalArticleItemSelectorViewDisplayContextTest.java
+++ modules/apps/journal/journal-web-test/src/testIntegration/java/com/liferay/journal/web/internal/display/test/JournalArticleItemSelectorViewDisplayContextTest.java
@@ -118,7 +118,7 @@ public class JournalArticleItemSelectorViewDisplayContextTest {
 			LogEntry logEntry = logEntries.get(0);
 
 			Assert.assertEquals(
-				"Unable to get classType for article: " +
+				"Unable to get class type for article: " +
 					journalArticle.getArticleId(),
 				logEntry.getMessage());
 		}
