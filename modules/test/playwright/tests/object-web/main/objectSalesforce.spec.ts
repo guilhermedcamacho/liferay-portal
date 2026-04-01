c438f8fda8964458c0fe37b0e83f6503aa2ba7f0
Guilherme Camacho <guilherme.camacho@liferay.com>
2026-04-01 16:02:18 -0300

    LPD-82336 SF

--- modules/test/playwright/tests/object-web/main/objectSalesforce.spec.ts
+++ modules/test/playwright/tests/object-web/main/objectSalesforce.spec.ts
@@ -12,7 +12,6 @@ import {instanceSettingsPagesTest} from '../../../fixtures/instanceSettingsPages
 import {isolatedSiteTest} from '../../../fixtures/isolatedSiteTest';
 import {loginTest} from '../../../fixtures/loginTest';
 import {objectPagesTest} from '../../../fixtures/objectPagesTest';
-import {getRandomInt} from '../../../utils/getRandomInt';
 import getRandomString from '../../../utils/getRandomString';
 import {waitForAlert} from '../../../utils/waitForAlert';
 import {generateObjectFields} from './utils/generateObjectFields';
@@ -20,128 +19,149 @@ import {generateObjectFields} from './utils/generateObjectFields';
 import {salesforceConfig} from './salesforce.config';
 
 const test = mergeTests(
-    dataApiHelpersTest,
-    featureFlagsTest({
-        'LPS-135430': {enabled: true},
-    }),
-    instanceSettingsPagesTest,
-    isolatedSiteTest,
-    loginTest(),
-    objectPagesTest
+	dataApiHelpersTest,
+	featureFlagsTest({
+		'LPS-135430': {enabled: true},
+	}),
+	instanceSettingsPagesTest,
+	isolatedSiteTest,
+	loginTest(),
+	objectPagesTest
 );
 
 test.beforeEach(async ({instanceSettingsPage, page}) => {
-    test.skip(
-        !salesforceConfig.salesforceLoginURL ||
-            !salesforceConfig.salesforceConsumerKey ||
-            !salesforceConfig.salesforceConsumerSecret ||
-            !salesforceConfig.salesforceUsername ||
-            !salesforceConfig.salesforcePassword,
-        'Requires Salesforce environment variables.'
-    );
+	test.skip(
+		!salesforceConfig.salesforceLoginURL ||
+			!salesforceConfig.salesforceConsumerKey ||
+			!salesforceConfig.salesforceConsumerSecret ||
+			!salesforceConfig.salesforceUsername ||
+			!salesforceConfig.salesforcePassword,
+		'Requires Salesforce environment variables.'
+	);
 
-    page.setViewportSize({height: 1080, width: 1920});
+	page.setViewportSize({height: 1080, width: 1920});
 
-    await test.step('Setup Salesforce Instance Settings', async () => {
-        await instanceSettingsPage.goToInstanceSetting(
-            'Third Party',
-            'Salesforce Integration'
-        );
+	await test.step('Setup Salesforce Instance Settings', async () => {
+		await instanceSettingsPage.goToInstanceSetting(
+			'Third Party',
+			'Salesforce Integration'
+		);
 
-        await page.getByLabel('Login URL').fill(salesforceConfig.salesforceLoginURL!);
-        await page.getByLabel('Consumer Key').fill(salesforceConfig.salesforceConsumerKey!);
-        await page.getByLabel('Consumer Secret').fill(salesforceConfig.salesforceConsumerSecret!);
-        await page.getByLabel('Username').fill(salesforceConfig.salesforceUsername!);
-        
-        await page.locator('input[name*="password"]')
-            .filter({ visible: true }) 
-            .fill(salesforceConfig.salesforcePassword!);
+		await page
+			.getByLabel('Login URL')
+			.fill(salesforceConfig.salesforceLoginURL!);
+		await page
+			.getByLabel('Consumer Key')
+			.fill(salesforceConfig.salesforceConsumerKey!);
+		await page
+			.getByLabel('Consumer Secret')
+			.fill(salesforceConfig.salesforceConsumerSecret!);
+		await page
+			.getByLabel('Username')
+			.fill(salesforceConfig.salesforceUsername!);
 
-        await instanceSettingsPage.saveAndWaitForAlert();
-    });
+		await page
+			.locator('input[name*="password"]')
+			.filter({visible: true})
+			.fill(salesforceConfig.salesforcePassword!);
+
+		await instanceSettingsPage.saveAndWaitForAlert();
+	});
 });
 
 test(
-    'LPS-162131 Assert CRUD with created custom object using Salesforce storage type',
-    {tag: '@LPS-162131'},
-    async ({apiHelpers, page, viewObjectEntriesPage}) => {
-        const objectFields = generateObjectFields({
-            objectFieldBusinessTypes: [{
-                businessType: 'Text',
-                externalReferenceCode: 'Title__c',
-                label: { en_US: 'Title' },
-                name: 'title',
-            }],
-        });
+	'LPS-162131 Assert CRUD with created custom object using Salesforce storage type',
+	{tag: '@LPS-162131'},
+	async ({apiHelpers, page, viewObjectEntriesPage}) => {
+		const objectFields = generateObjectFields({
+			objectFieldBusinessTypes: [
+				{
+					businessType: 'Text',
+					externalReferenceCode: 'Title__c',
+					label: {en_US: 'Title'},
+					name: 'title',
+				},
+			],
+		});
 
-    const objectDefinitionAPIClient = await apiHelpers.buildRestClient(ObjectDefinitionAPI);
+		const objectDefinitionAPIClient =
+			await apiHelpers.buildRestClient(ObjectDefinitionAPI);
 
-    const {body: objectDefinition} = await objectDefinitionAPIClient.postObjectDefinition({
-                                        active: true,
-                                        externalReferenceCode: 'Playwright_Test__c',
-                                        label: { en_US: "Playwright Test" },
-                                        name: "PlaywrightTest",
-                                        objectFields,
-                                        panelCategoryKey: 'control_panel.object',
-                                        pluralLabel: { en_US: "Playwright Tests" },
-                                        portlet: true,
-                                        scope: 'company',
-                                        status: { code: 0 },
-                                        storageType: 'salesforce',
-                            });
+		const {body: objectDefinition} =
+			await objectDefinitionAPIClient.postObjectDefinition({
+				active: true,
+				externalReferenceCode: 'Playwright_Test__c',
+				label: {en_US: 'Playwright Test'},
+				name: 'PlaywrightTest',
+				objectFields,
+				panelCategoryKey: 'control_panel.object',
+				pluralLabel: {en_US: 'Playwright Tests'},
+				portlet: true,
+				scope: 'company',
+				status: {code: 0},
+				storageType: 'salesforce',
+			});
 
-    apiHelpers.data.push({
-        id: objectDefinition.id,
-        type: 'objectDefinition',
-    });
+		apiHelpers.data.push({
+			id: objectDefinition.id,
+			type: 'objectDefinition',
+		});
 
-    const objectFieldValue = getRandomString();
-    const objectFieldUpdatedValue = getRandomString();
+		const objectFieldValue = getRandomString();
+		const objectFieldUpdatedValue = getRandomString();
 
-    await test.step('Create Object Entry', async () => {
-        await viewObjectEntriesPage.goto(objectDefinition.className);
-        await viewObjectEntriesPage.clickAddObjectEntry(objectDefinition.label['en_US']);
+		await test.step('Create Object Entry', async () => {
+			await viewObjectEntriesPage.goto(objectDefinition.className);
+			await viewObjectEntriesPage.clickAddObjectEntry(
+				objectDefinition.label['en_US']
+			);
 
-        await viewObjectEntriesPage.fillObjectEntry({
-            objectFieldBusinessType: 'Text',
-            objectFieldLabel: 'Title',
-            objectFieldValue: objectFieldValue,
-        });
+			await viewObjectEntriesPage.fillObjectEntry({
+				objectFieldBusinessType: 'Text',
+				objectFieldLabel: 'Title',
+				objectFieldValue: objectFieldValue,
+			});
 
-        await viewObjectEntriesPage.saveObjectEntryButton.click();
-        await waitForAlert(page);
-        await viewObjectEntriesPage.backButton.click();
-    }); 
+			await viewObjectEntriesPage.saveObjectEntryButton.click();
+			await waitForAlert(page);
+			await viewObjectEntriesPage.backButton.click();
+		});
 
-    await test.step('Read Object Entry', async () => {
-        await expect(page.getByRole('cell', { name: objectFieldValue })).toBeVisible();
-    });
+		await test.step('Read Object Entry', async () => {
+			await expect(
+				page.getByRole('cell', {name: objectFieldValue})
+			).toBeVisible();
+		});
 
-    await test.step('Update Object Entry', async () => {
-        await page.getByRole('button', {name: 'Actions'}).last().click();
-        await page.getByRole('menuitem', {name: 'View'}).click();
+		await test.step('Update Object Entry', async () => {
+			await page.getByRole('button', {name: 'Actions'}).last().click();
+			await page.getByRole('menuitem', {name: 'View'}).click();
 
-        await viewObjectEntriesPage.fillObjectEntry({
-            objectFieldBusinessType: 'Text',
-            objectFieldLabel: 'Title',
-            objectFieldValue: objectFieldUpdatedValue,
-        });
+			await viewObjectEntriesPage.fillObjectEntry({
+				objectFieldBusinessType: 'Text',
+				objectFieldLabel: 'Title',
+				objectFieldValue: objectFieldUpdatedValue,
+			});
 
-        await viewObjectEntriesPage.saveObjectEntryButton.click();
-        await expect(viewObjectEntriesPage.successMessage).toBeVisible();
-        await viewObjectEntriesPage.backButton.click();
+			await viewObjectEntriesPage.saveObjectEntryButton.click();
+			await expect(viewObjectEntriesPage.successMessage).toBeVisible();
+			await viewObjectEntriesPage.backButton.click();
 
-        await expect(page.getByRole('cell', { name: objectFieldUpdatedValue })).toBeVisible();
-    });
+			await expect(
+				page.getByRole('cell', {name: objectFieldUpdatedValue})
+			).toBeVisible();
+		});
 
-    await test.step('Delete Object Entry', async () => {
-        await viewObjectEntriesPage.frontendDatasetActions.last().click();
-        await viewObjectEntriesPage.frontendDatasetDeleteAction.click();
-        await viewObjectEntriesPage.deletionConfirmationModal
-            .getByRole('button', { name: 'Delete' })
-            .click();
+		await test.step('Delete Object Entry', async () => {
+			await viewObjectEntriesPage.frontendDatasetActions.last().click();
+			await viewObjectEntriesPage.frontendDatasetDeleteAction.click();
+			await viewObjectEntriesPage.deletionConfirmationModal
+				.getByRole('button', {name: 'Delete'})
+				.click();
 
-        await expect(page.getByRole('cell', { name: objectFieldUpdatedValue })).toBeAttached({attached: false});
-    });
-    }
+			await expect(
+				page.getByRole('cell', {name: objectFieldUpdatedValue})
+			).toBeAttached({attached: false});
+		});
+	}
 );
--- modules/test/playwright/tests/object-web/main/salesforce.config.ts
+++ modules/test/playwright/tests/object-web/main/salesforce.config.ts
@@ -4,21 +4,22 @@
  */
 
 const salesforceConfig = {
-		salesforceConsumerKey: process.env.OBJECT_STORAGE_SALESFORCE_CONSUMER_KEY
-			? process.env.OBJECT_STORAGE_SALESFORCE_CONSUMER_KEY
-			: '',
-		salesforceConsumerSecret: process.env.OBJECT_STORAGE_SALESFORCE_CONSUMER_SECRET
-			? process.env.OBJECT_STORAGE_SALESFORCE_CONSUMER_SECRET
-			: '',
-		salesforceLoginURL: process.env.OBJECT_STORAGE_SALESFORCE_LOGIN_URL
-			? process.env.OBJECT_STORAGE_SALESFORCE_LOGIN_URL
-			: 'https://test.salesforce.com/',
-		salesforcePassword: process.env.OBJECT_STORAGE_SALESFORCE_PASSWORD
-			? process.env.OBJECT_STORAGE_SALESFORCE_PASSWORD
-			: '',
-		salesforceUsername: process.env.OBJECT_STORAGE_SALESFORCE_USERNAME
-			? process.env.OBJECT_STORAGE_SALESFORCE_USERNAME
-			: '',
+	salesforceConsumerKey: process.env.OBJECT_STORAGE_SALESFORCE_CONSUMER_KEY
+		? process.env.OBJECT_STORAGE_SALESFORCE_CONSUMER_KEY
+		: '',
+	salesforceConsumerSecret: process.env
+		.OBJECT_STORAGE_SALESFORCE_CONSUMER_SECRET
+		? process.env.OBJECT_STORAGE_SALESFORCE_CONSUMER_SECRET
+		: '',
+	salesforceLoginURL: process.env.OBJECT_STORAGE_SALESFORCE_LOGIN_URL
+		? process.env.OBJECT_STORAGE_SALESFORCE_LOGIN_URL
+		: 'https://test.salesforce.com/',
+	salesforcePassword: process.env.OBJECT_STORAGE_SALESFORCE_PASSWORD
+		? process.env.OBJECT_STORAGE_SALESFORCE_PASSWORD
+		: '',
+	salesforceUsername: process.env.OBJECT_STORAGE_SALESFORCE_USERNAME
+		? process.env.OBJECT_STORAGE_SALESFORCE_USERNAME
+		: '',
 };
 
 export {salesforceConfig};
