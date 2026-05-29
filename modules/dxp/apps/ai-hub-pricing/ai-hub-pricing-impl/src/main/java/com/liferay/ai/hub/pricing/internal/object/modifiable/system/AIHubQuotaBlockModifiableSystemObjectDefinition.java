901d1cbc5540e4388e309d6e8d00f2a31a55554e
Carolina Barbosa <carolina.barbosa@liferay.com>
2026-05-29 09:30:27 -0300

    LPD-91669 Implement ModifiableSystemObjectDefinition for AIHubQuotaConversionTable and AIHubQuotaBlock object definitions

---
+++ modules/dxp/apps/ai-hub-pricing/ai-hub-pricing-impl/src/main/java/com/liferay/ai/hub/pricing/internal/object/modifiable/system/AIHubQuotaBlockModifiableSystemObjectDefinition.java
@@ -0,0 +1,27 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.ai.hub.pricing.internal.object.modifiable.system;
+
+import com.liferay.object.modifiable.system.ModifiableSystemObjectDefinition;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Carolina Barbosa
+ */
+@Component(
+	property = "object.definition.name=AIHubQuotaBlock",
+	service = ModifiableSystemObjectDefinition.class
+)
+public class AIHubQuotaBlockModifiableSystemObjectDefinition
+	implements ModifiableSystemObjectDefinition {
+
+	@Override
+	public String getRESTContextPath() {
+		return "/ai-hub/quota-blocks";
+	}
+
+}
