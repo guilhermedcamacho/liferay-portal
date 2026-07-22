a7ed145946ab548370944cdb151d97d1d35a847b
Guilherme Camacho <guilherme.camacho@liferay.com>
2026-07-22 16:17:37 -0300

    LPD-97811 Test the task-asset link cascade delete

--- modules/apps/site/site-cmp-site-initializer-test/src/testIntegration/java/com/liferay/site/cmp/site/initializer/internal/service/test/ObjectEntryLocalServiceTest.java
+++ modules/apps/site/site-cmp-site-initializer-test/src/testIntegration/java/com/liferay/site/cmp/site/initializer/internal/service/test/ObjectEntryLocalServiceTest.java
@@ -77,6 +77,26 @@ public class ObjectEntryLocalServiceTest {
 				projectAssetRelationshipObjectEntry.getObjectEntryId()));
 	}
 
+	@Test
+	public void testDeleteTaskDeletesTaskAssetRelationships() throws Exception {
+		ObjectEntry taskObjectEntry = CMPTestUtil.addTaskObjectEntry();
+
+		_depotEntry = _depotEntryLocalService.fetchGroupDepotEntry(
+			taskObjectEntry.getGroupId());
+
+		ObjectEntry taskAssetRelationshipObjectEntry =
+			_addTaskAssetRelationshipObjectEntry(
+				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+				RandomTestUtil.randomString(), taskObjectEntry);
+
+		_objectEntryLocalService.deleteObjectEntry(
+			taskObjectEntry.getObjectEntryId());
+
+		Assert.assertNull(
+			_objectEntryLocalService.fetchObjectEntry(
+				taskAssetRelationshipObjectEntry.getObjectEntryId()));
+	}
+
 	private ObjectEntry _addProjectAssetRelationshipObjectEntry(
 			String classExternalReferenceCode, String className,
 			String groupExternalReferenceCode, ObjectEntry projectObjectEntry)
@@ -100,6 +120,33 @@ public class ObjectEntryLocalServiceTest {
 			).put(
 				"r_cmpProjectToCMPProjectAssetRelationships_c_cmpProjectId",
 				projectObjectEntry.getObjectEntryId()
+			).build(),
+			ServiceContextTestUtil.getServiceContext());
+	}
+
+	private ObjectEntry _addTaskAssetRelationshipObjectEntry(
+			String classExternalReferenceCode, String className,
+			String groupExternalReferenceCode, ObjectEntry taskObjectEntry)
+		throws Exception {
+
+		ObjectDefinition objectDefinition =
+			_objectDefinitionLocalService.
+				getObjectDefinitionByExternalReferenceCode(
+					"L_CMP_TASK_ASSET_RELATIONSHIP",
+					TestPropsValues.getCompanyId());
+
+		return _objectEntryLocalService.addObjectEntry(
+			taskObjectEntry.getGroupId(), taskObjectEntry.getUserId(),
+			objectDefinition.getObjectDefinitionId(), 0, null,
+			HashMapBuilder.<String, Serializable>put(
+				"classExternalReferenceCode", classExternalReferenceCode
+			).put(
+				"className", className
+			).put(
+				"groupExternalReferenceCode", groupExternalReferenceCode
+			).put(
+				"r_cmpTaskToCMPTaskAssetRelationships_c_cmpTaskId",
+				taskObjectEntry.getObjectEntryId()
 			).build(),
 			ServiceContextTestUtil.getServiceContext());
 	}
